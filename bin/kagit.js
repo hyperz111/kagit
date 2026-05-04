26fc975e8cc6d71c6b8b4f5d39d661256ad46985
hyperz111 <firzeroun11@gmail.com>
2026-05-04 23:41:06 +0700

    fix: recursive remove and use right skip handler

--- bin/kagit.js
+++ bin/kagit.js
@@ -5,7 +5,6 @@ import p from 'path'
 let k = 'package.json',
   g = '.git',
   n = ' not found',
-  s = '#!/bin/sh\n[ ! "$KAGIT_SKIP_HOOKS" ] && ',
   x = process.exit,
   e = console.error,
   d = p.resolve(g, 'hooks')
@@ -14,9 +13,9 @@ if (!process.env.KAGIT_SKIP_INSTALL) {
   f.existsSync(g) || e(g + n) || x(1)
   f.existsSync(k) || e(k + n) || x(1)
 
-  f.rmSync(d, {force: true})
+  f.rmSync(d, {force: true, recursive: true})
   f.mkdirSync(d, {recursive: true})
 
   k = JSON.parse(f.readFileSync(k, 'utf8')).kagit || {}
-  for (g in k) f.writeFileSync(p.join(d, g), s + k[g], {mode: 0o755})
+  for (g in k) f.writeFileSync(p.join(d, g), '#!/bin/sh\n[ "$KAGIT_SKIP_HOOKS" ] && exit\n' + k[g], {mode: 0o755})
 }
