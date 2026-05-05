c7f8352b3aa2dcd71b812a87a37b1bddb4116fb2
hyperz111 <firzeroun11@gmail.com>
2026-05-06 03:49:32 +0700

    refactor: create diy clean-pusblish

---
+++ scripts/clean-publish.js
@@ -0,0 +1,19 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { spawnSync } from "node:child_process";
+
+const destination = fs.mkdtempSync(path.join(import.meta.dirname, "cp-"));
+fs.cpSync(path.join(import.meta.dirname, "..", "bin"), path.join(destination, "bin"), { recursive: true });
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(import.meta.dirname, "..", "package.json"), "utf8"));
+
+delete packageJson.scripts;
+delete packageJson.devDependencies;
+delete packageJson.packageManager;
+delete packageJson.kagit;
+
+fs.writeFileSync(path.join(destination, "package.json"), JSON.stringify(packageJson));
+
+spawnSync("pnpm", ["publish"], { stdio: "inherit", cwd: destination });
+
+fs.rmSync(destination, { recursive: true, force: true });
